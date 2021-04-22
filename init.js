5303b94b77d54fc9d2e2676d3a3b0c013c992407
jag73 <jag73@aber.ac.uk>
2021-04-22 01:12:29 +0100

    Final push - completed test

--- init.js
+++ init.js
@@ -1,29 +1,36 @@
 // Run when loading is complete
 $(document).ready(function () {
-    // Initialise the carousel
+    /* Initialise the carousel with initial settings and breakpoint
+       settings for responsive design
+    */
     $('.carousel').slick({
         autoplay: true,
         pauseOnHover: true,
         mobileFirst: true,
+        fade: true,
+        slidesToShow: 1,
         responsive: [
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3
+                    slidesToShow: 3,
+                    fade: false,
                 }
             },
             {
                 breakpoint: 768,
                 settings: {
                     slidesToShow: 2,
+                    fade: false,
                 }
             }
         ]
 
     });
-    // Toggle answers - Get sibling of button and toggle it
+    // Toggle answers - Gets only sibling of button and toggles it
     $('.open-answer').click(function () {
         $(this).siblings().toggle();
     })
         ;
+
 });
